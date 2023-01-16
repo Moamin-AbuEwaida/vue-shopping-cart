2ff57aaeeba5257773f0d197a53a5ba8132ae090
Moamin <moamin.salamah@gmail.com>
2023-01-16 14:59:16 +0100

    right side bar component

---
+++ src/components/RightSideBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-navigation-drawer
+    drawer1
+    app
+    color="#000"
+    mini-variant
+    mini-variant-width="130"
+    right
+  >
+    <v-avatar class="d-block text-center mx-auto mt-4 mb-10" size="80">
+      <v-btn class="ma-2" outlined large fab color="white">SH</v-btn>
+    </v-avatar>
+    <v-card flat color="#6f0cff" class="rounded-xl mx-4 py-10 text-center top">
+      <v-btn icon dark class="mb-5">
+        <v-icon>fas fa-sort-amount-up-alt</v-icon> </v-btn
+      ><br />
+      <span class="white--text">Create<br />Order</span>
+    </v-card>
+    <div
+      style="
+        position: absolute;
+        bottom: 20px;
+        margin-left: auto;
+        margin-right: auto;
+        left: 0;
+        right: 0;
+        text-align: center;
+      "
+    >
+      <v-btn dark class="chargewith mb-1"> <v-icon>mdi-bell</v-icon> </v-btn
+      ><br />
+      <v-badge bordered color="#6f0cff" dot overlap>
+        <v-btn dark class="chargewith">
+          <v-icon class="">mdi-message</v-icon>
+        </v-btn>
+      </v-badge>
+    </div>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    selectedItem: 0,
+    drawer1: null,
+  }),
+};
+</script>
+
+<style>
+.v-btn:not(.v-btn--round).v-size--default.changewith {
+  height: 45px !important;
+  min-width: 30px !important;
+  padding: 0 12px;
+}
+.top {
+  margin-top: 150px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,124 @@
 <template>
   <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
-    <sideBar />
+    <SideBar />
+    <RightSideBar />
   </v-app>
 </template>
 
 <script>
+import RightSideBar from "../components/RightSideBar.vue";
 import SideBar from "../components/SideBar.vue";
 export default {
   name: "HomePage",
-
-  components: { SideBar },
+  data: () => ({
+    selection: 1,
+    drawer: true,
+    shoes: [
+      {
+        image: "3.jpg",
+        pic: "1.png",
+        price: "$465",
+        id: "ID: CK1911159967352",
+        date: "02.12.2021, 3:34 AM",
+        marque: "Nike Air MAx",
+        pay1: "LUXEMBURG",
+        pay2: "ITALY",
+        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+      },
+      {
+        image: "9.jfif",
+        pic: "2.png",
+        price: "$714",
+        id: "ID: CK1911159967621",
+        date: "02.12.2021, 4:12 AM",
+        marque: "AIR Jordon",
+        pay1: "FUTURE",
+        pay2: "WAR",
+        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+      },
+      {
+        image: "8.jpg",
+        pic: "3.png",
+        price: "$982",
+        id: "ID: CK1911159967532",
+        date: "02.12.2021, 6:24 AM",
+        marque: "Adidas GR",
+        pay1: "LONDON",
+        pay2: "ALGERIA",
+        avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+      },
+      {
+        image: "7.jpg",
+        pic: "4.png",
+        price: "$394",
+        id: "ID: CK1911159968663",
+        date: "02.12.2021, 7:51 AM",
+        marque: "Puma New",
+        pay1: "SRILANKA",
+        pay2: "PALAU",
+        avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
+      },
+    ],
+    orders: [
+      {
+        pic: "5.png",
+        price: "€465",
+      },
+      {
+        pic: "6.png",
+        price: "€714",
+      },
+      {
+        pic: "7.png",
+        price: "€982",
+      },
+      {
+        pic: "8.png",
+        price: "€394",
+      },
+    ],
+  }),
+  components: {
+    SideBar,
+    RightSideBar,
+  },
+  methods: {},
 };
 </script>
+
+<style scoped>
+.v-tab.withoutupercase {
+  text-transform: none !important;
+}
+.v-tabs {
+  width: 50% !important;
+}
+.v-btn.withoutupercase {
+  text-transform: none !important;
+}
+/*div{
+  display:inline-block;
+  float:left;
+  color:#fff;
+  font-size:10px;
+}*/
+.three {
+  width: 50px;
+  height: 50px;
+}
+.four {
+  width: 50px;
+  height: 25px;
+  background: black;
+}
+.five {
+  width: 50px;
+  height: 25px;
+  background: #042a0f;
+}
+.six {
+  width: 50px;
+  height: 25px;
+  background: #2c2107;
+}
+</style>
