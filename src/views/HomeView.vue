68482cf925e1b83f10655bc368d9d14cee921d76
Moamin <moamin.salamah@gmail.com>
2023-01-16 16:19:13 +0100

    main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,213 @@
 <template>
   <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
-    <SideBar />
+    <SideBar :drawer="drawer" />
     <RightSideBar />
+    <v-container>
+      <v-toolbar flat color="transparent">
+        <v-btn
+          icon
+          dark
+          class="d-lg-none d-xl-flex"
+          @click.stop="drawer = !drawer"
+        >
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-text-field
+          label="Search on maufarm"
+          class="pt-5 d-none d-sm-flex"
+          dark
+          filled
+          prepend-inner-icon="mdi-magnify"
+          solo
+          flat
+          background-color="transparent"
+          rounded
+          outlined
+        >
+        </v-text-field>
+        <v-spacer></v-spacer>
+        <v-btn dark class="mr-2">
+          <v-icon left>fas fa-filter</v-icon> Filter
+        </v-btn>
+        <v-btn dark class="mr-2">
+          <v-icon left>fas fa-upload</v-icon> Export
+        </v-btn>
+      </v-toolbar>
+      <h3 class="white--text">SALES</h3>
+      <p class="grey--text">
+        A Great Way To Generate All The Motivation You Need To Get Fit
+      </p>
+      <v-app-bar dark color="transparent" flat>
+        <v-tabs color="#6f0cff">
+          <v-tab class="withoutupercase">Fultillment Status View</v-tab>
+          <v-tab class="withoutupercase">Real-time Trackning</v-tab>
+          <v-tab class="withoutupercase">Sales View</v-tab>
+          <v-tab class="withoutupercase">Balances</v-tab>
+          <v-tab class="withoutupercase">Transactions</v-tab>
+          <v-tab class="withoutupercase">Engagement Rate</v-tab>
+        </v-tabs>
+        <v-spacer></v-spacer>
+        <v-btn dark text class="d-none d-sm-flex">
+          <v-icon left>mdi-magnify</v-icon>
+          SEARCH
+        </v-btn>
+        <v-btn rounded color="white" class="black--text">
+          List
+          <v-icon right>mdi-format-list-bulleted</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-apps</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-divider color="grey"></v-divider>
+      <v-toolbar flat color="transparent">
+        <v-divider vertical color="green" inset></v-divider>
+        <v-toolbar-title class="white--text ml-4">Alert</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-chip class="ma-1 d-none d-sm-flex" label text-color="white" dark>
+          <v-avatar color="white" rounded class="mr-2">
+            <v-img src="1.png" contain></v-img>
+          </v-avatar>
+          Nike new golf
+        </v-chip>
+        <v-chip class="ma-1 d-none d-sm-flex" label text-color="white" dark>
+          <v-avatar color="white" rounded class="mr-2">
+            <v-img src="2.png" contain></v-img>
+          </v-avatar>
+          Sneakers printemps
+        </v-chip>
+        <v-spacer></v-spacer>
+        <v-btn dark class="mr-2 withoutupercase d-none d-sm-flex"
+          >See All (10)</v-btn
+        >
+        <v-btn dark class="mr-2" outlined>
+          <v-icon left>mdi-chevron-left</v-icon>
+          <v-icon right>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-row class="mt-n14">
+        <v-col
+          cols="12"
+          xs="12"
+          sm="6"
+          md="4"
+          lg="3"
+          v-for="(shoe, i) in shoes"
+          :key="i"
+        >
+          <v-card
+            class="mx-auto mt-12 rounded-xl"
+            max-width="374"
+            color="#151515"
+          >
+            <v-img height="170" :src="shoe.image"></v-img>
+            <v-toolbar color="transparent" class="mt-n7" flat>
+              <v-avatar color="white" rounded class="mr-2">
+                <v-img :src="shoe.pic" contain></v-img>
+              </v-avatar>
+              <v-spacer></v-spacer>
+              <v-avatar color="black" rounded class="mr-2" dark>
+                <div class="three">
+                  <div class="four">
+                    <span class="white--text caption">{{ shoe.price }}</span>
+                  </div>
+                  <div class="five">
+                    <span class="green--text" caption>HOLD</span>
+                  </div>
+                </div>
+              </v-avatar>
+            </v-toolbar>
+            <v-card-title class="grey--text caption">{{
+              shoe.id
+            }}</v-card-title>
+            <v-card-title class="grey--text text-grey-darken-1 caption mt-n6">{{
+              shoe.date
+            }}</v-card-title>
+            <v-card-text class="white--text text-grey-darken-1 mt-n2">{{
+              shoe.marque
+            }}</v-card-text>
+            <v-card-text class="mt-n4">
+              <v-chip-group
+                active-class="deep-purple accent-4 white--text"
+                column
+              >
+                <v-chip label dark>{{ shoe.pay1 }}</v-chip>
+                <v-chip label dark>{{ shoe.pay2 }}</v-chip>
+                <v-spacer></v-spacer>
+                <v-avatar size="40">
+                  <v-img :src="shoe.avatar"></v-img>
+                </v-avatar>
+              </v-chip-group>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-toolbar flat color="transparent" class="mt-n4">
+        <v-divider vertical color="orange" inset></v-divider>
+        <v-toolbar-title class="white--text ml-4">Recent Order</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-chip class="ma-1 d-none d-sm-flex" label text-color="white" dark>
+          <v-avatar color="white" rounded class="mr-2">
+            <v-img src="2.png" contain></v-img>
+          </v-avatar>
+          Nike New
+        </v-chip>
+        <v-chip class="ma-1 d-none d-sm-flex" label text-color="white" dark>
+          <v-avatar color="white" rounded class="mr-2">
+            <v-img src="3.png" contain></v-img>
+          </v-avatar>
+          Zoom JD
+        </v-chip>
+        <v-chip class="ma-1 d-none d-sm-flex" label text-color="white" dark>
+          <v-avatar color="white" rounded class="mr-2">
+            <v-img src="4.png" contain></v-img>
+          </v-avatar>
+          Jordan B2
+        </v-chip>
+        <v-spacer></v-spacer>
+        <v-btn dark class="mr-2 withouupercase d-none s-sm-flex">
+          See All [129]
+        </v-btn>
+        <v-btn dark class="mr-2" outlined>
+          <v-icon left>mdi-chevron-left</v-icon>
+          <v-icon right>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-row class="mt-n10">
+        <v-col
+          cols="12"
+          xs="12"
+          sm="6"
+          md="3"
+          v-for="(order, i) in orders"
+          :key="i"
+        >
+          <v-card
+            class="mx-auto my-12 rounded-lg"
+            max-width="374"
+            color="#151515"
+          >
+            <v-toolbar color="transparent" class="mt-n6" flat>
+              <v-avatar color="white" rounded class="mr-2">
+                <v-img :src="order.pic" contain></v-img>
+              </v-avatar>
+              <v-spacer></v-spacer>
+              <v-avatar color="black" rounded class="mr-2" dark>
+                <div class="three">
+                  <div class="four">
+                    <span class="white--text caption">{{ order.price }}</span>
+                  </div>
+                  <div class="six">
+                    <span class="orange--text caption">HOLD</span>
+                  </div>
+                </div>
+              </v-avatar>
+            </v-toolbar>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
