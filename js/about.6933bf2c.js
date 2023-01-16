7b6aa0bfaf18147a2ffd510ed67babf707e0a165
Moamin <moamin.salamah@gmail.com>
2023-01-16 16:24:34 +0100

    pushing dist folder

---
+++ js/about.6933bf2c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_shopping_cart"]=self["webpackChunkvue_shopping_cart"]||[]).push([[443],{7750:function(n,t,u){u.r(t),u.d(t,{default:function(){return c}});var s=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],a=u(1001),r={},i=(0,a.Z)(r,s,e,!1,null,null,null),c=i.exports}}]);
+//# sourceMappingURL=about.6933bf2c.js.map
