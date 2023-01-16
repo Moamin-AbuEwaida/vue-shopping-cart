59329b4a5a24c4a5128e0145d62640afe5a4b7af
Moamin <moamin.salamah@gmail.com>
2023-01-16 12:51:34 +0100

    side bar component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>hello</div>
+  <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
+    <sideBar />
+  </v-app>
 </template>
 
 <script>
+import SideBar from "../components/SideBar.vue";
 export default {
   name: "HomePage",
 
-  components: {},
+  components: { SideBar },
 };
 </script>
